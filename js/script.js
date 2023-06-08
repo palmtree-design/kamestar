8ccefeff139da68021f1177ea536c3d2fb4d99dd
palmtree-design <130032978+palmtree-design@users.noreply.github.com>
2023-06-08 20:36:26 +0900

    動き

---
+++ js/script.js
@@ -0,0 +1,20 @@
+'use strict';
+// 動きのきっかけの起点となるアニメーションの名前を定義
+function fadeAnime() {
+
+    // ふわっ
+    $('.fadeUpTrigger').each(function () { //fadeUpTriggerというクラス名が
+        var elemPos = $(this).offset().top - 80; //要素より、50px上の
+        var scroll = $(window).scrollTop();
+        var windowHeight = $(window).height();
+        if (scroll >= elemPos - windowHeight) {
+            $(this).addClass('fadeUp'); // 画面内に入ったらfadeUpというクラス名を追記
+        }
+    });
+}
+
+
+// 画面をスクロールをしたら動かしたい場合の記述
+$(window).scroll(function () {
+    fadeAnime(); /* アニメーション用の関数を呼ぶ*/
+}); // ここまで画面をスクロールをしたら動かしたい場合の記述
