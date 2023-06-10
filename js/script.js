e7cfb1ff4607472136a0e8bd70442da974c98de2
palmtree-design <130032978+palmtree-design@users.noreply.github.com>
2023-06-11 02:49:28 +0900

    JS修正　furuta

--- js/script.js
+++ js/script.js
@@ -11,17 +11,19 @@ function fadeAnime() {
             $(this).addClass('fadeUp'); // 画面内に入ったらfadeUpというクラス名を追記
         }
     });
+  }
     $('.fadeInTopRightTrigger').on('inview', function(event, isInView) {
         if (isInView) {//表示領域に入った時1度だけアニメーションをさせる
           $(this).addClass('animate__animated animate__fadeInTopRight animate__slower');//クラス名が付与
         }
       });
+
       $('.jelloTrigger').on('inview', function(event, isInView) {
         if (isInView) {//表示領域に入った時1度だけアニメーションをさせる
           $(this).addClass('animate__animated animate__jello');//クラス名が付与
         }
       });
-}
+    
 
 
 // 画面をスクロールをしたら動かしたい場合の記述
